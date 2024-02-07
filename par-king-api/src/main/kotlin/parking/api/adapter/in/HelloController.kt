eb4c5cdb44cc886f89576bbd11dd4621e585b197
hungry <bob@gim-wontaes-MacBook-Pro.local>
2024-02-07 18:46:42 +0900

    Hello Controller 추가, api-local.yml 추가
    /hello 로 들어올 경우 helloName 반환하도록 추가

---
+++ par-king-api/src/main/kotlin/parking/api/adapter/in/HelloController.kt
@@ -0,0 +1,16 @@
+package parking.api.adapter.`in`
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController(
+    @Value("\${hello.name}")
+    val helloName: String
+) {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return helloName
+    }
+}
