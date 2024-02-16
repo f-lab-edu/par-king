4174a8e123056ff88ddc1a5597df93babaecd0cd
hungry <bob@gim-wontaes-MacBook-Pro.local>
2024-02-16 18:35:06 +0900

    jpa 사용을 위한 설정 추가와 flyway 기능 추가 및 테스트 하기 위한 Member 테이블 추가 + querydsl 설정 추가

---
+++ persistence/src/main/kotlin/jpa/entity/MemberJpaEntity.kt
@@ -0,0 +1,48 @@
+package jpa.entity
+
+import domain.entity.Member
+import domain.entity.MemberInfo
+import domain.entity.MemberStatus
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "member")
+data class MemberJpaEntity (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "member_id")
+    val memberId: String,
+    @Column(name = "member_name")
+    val memberName: String,
+    val email: String? = null,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_status")
+    val memberStatus: MemberStatus = MemberStatus.ACTIVATED,
+
+    @Column(name="no_show_count")
+    val noShowCount: Long? = null,
+    @Column(name = "start_no_show_time")
+    val startNoShowTime: LocalDateTime? = null
+
+) : BaseEntity() {
+    fun to() = Member (
+        memberId = this.id,
+        memberInfo = MemberInfo(this.memberId, this.memberName, this.email),
+        memberStatus = this.memberStatus
+    )
+
+    companion object {
+        fun from(member: Member) = MemberJpaEntity(
+            memberId = member.memberInfo.memberId,
+            memberName = member.memberInfo.name,
+            email = member.memberInfo.email,
+            memberStatus = member.memberStatus,
+            noShowCount = member.noShowCount,
+            startNoShowTime = member.startNoShowTime
+        )
+    }
+}
