2f0a03090f75882d2cffe13ef3a46a1b9ff5bdfb
acecic82 <102938al@naver.com>
2024-09-29 00:09:48 +0900

    memberId로 소유한 car 정보들을 조회하는 service test 추가

--- api/src/test/kotlin/com/parking/api/application/service/CarInquiryServiceTest.kt
+++ api/src/test/kotlin/com/parking/api/application/service/CarInquiryServiceTest.kt
@@ -6,6 +6,7 @@ import com.parking.api.application.port.out.FindParkingLotPort
 import com.parking.api.application.vo.ResponseCarInfoVO
 import com.parking.domain.entity.Car
 import com.parking.domain.exception.CarException
+import com.parking.domain.exception.MemberException
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
@@ -23,6 +24,8 @@ class CarInquiryServiceTest : BehaviorSpec() {
 
         val carInquiryService = CarInquiryService(findCarPort, findParkingLotPort, findMemberPort)
 
+        val car = Car(1, "가1234", 1L)
+
         Given("carId 가 있는 경우") {
             val carId = 1L
 
@@ -38,7 +41,6 @@ class CarInquiryServiceTest : BehaviorSpec() {
             }
 
             When("정보 조회 시 정보가 있는 경우") {
-                val car = Car(1, "가1234", 1L)
 
                 every { findCarPort.findById(carId) } returns car
 
@@ -48,6 +50,50 @@ class CarInquiryServiceTest : BehaviorSpec() {
                     val response = carInquiryService.findById(carId)
 
                     Assertions.assertEquals(desireCarInfo, response)
+                }
+            }
+        }
+
+        Given("memberId가 주어진 경우") {
+            val memberId = "user1"
+
+            When("현재 session 의 user 와 일치하지 않을 때") {
+                val currentUser = "user2"
+
+                Then("Exception 이 나와야 한다.") {
+                    assertThrows<MemberException> {
+                        carInquiryService.findAllByMemberId(currentUser, memberId)
+                    }
+                }
+            }
+
+            When("현재 session 의 user 와 일치하지만, memberId 가 존재하지 않을 때") {
+                val currentUser = "user1"
+
+                every { findMemberPort.findIdByMemberId(memberId) } returns null
+
+                Then("Exception 발생") {
+                    assertThrows<MemberException> {
+                        carInquiryService.findAllByMemberId(currentUser, memberId)
+                    }
+                }
+            }
+
+            When("memberId 로 member 정보가 없을 때") {
+                val currentUser = "user1"
+                val id = 1L
+
+                every { findMemberPort.findIdByMemberId(memberId) } returns id
+                every { findCarPort.findAllByMemberId(id) } returns listOf(car)
+
+                Then("응답 반환") {
+                    val deSireResult = listOf(ResponseCarInfoVO.from(car, null))
+
+                    val realResult = carInquiryService.findAllByMemberId(currentUser, memberId)
+
+                    realResult.forEachIndexed { index, responseCarInfoVO ->
+                        Assertions.assertEquals(responseCarInfoVO, deSireResult.get(index))
+                    }
                 }
             }
         }
