7145c67fbab1d21ed23987db4c94c066882a1853
acecic82 <102938al@naver.com>
2024-10-26 00:09:00 +0900

    ParkingLotCommandController parkingLot 만드는 부분 test 추가

--- api/src/test/kotlin/com/parking/api/adapter/in/ParkingLotCommandControllerTest.kt
+++ api/src/test/kotlin/com/parking/api/adapter/in/ParkingLotCommandControllerTest.kt
@@ -1,15 +1,24 @@
 package com.parking.api.adapter.`in`
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.parking.api.adapter.`in`.dto.ParkingLotInfoDTO
 import com.parking.api.application.port.`in`.parkingLot.CreateParkingLotUseCase
 import com.parking.api.application.port.`in`.parkingLot.DeleteParkingLotUseCase
 import com.parking.api.application.port.`in`.parkingLot.ModifyParkingLotUseCase
+import com.parking.api.application.vo.ParkingLotInfoVO
 import com.parking.api.common.advice.ParkingAdvice
+import com.parking.api.common.dto.SuccessResponseDTO
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
 import io.mockk.mockk
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @SpringBootTest
@@ -33,6 +42,41 @@ class ParkingLotCommandControllerTest(
                 .build()
         }
 
+        this.describe("ParkingLotCommandController Test") {
+            context("생성하는 경우") {
+                val parkingLotInfoVO = ParkingLotInfoVO(
+                    "User1",
+                    parkingLotId = 1L,
+                    name = "주차장1",
+                    totalSpace = 10L,
+                    cityName = "Seoul",
+                    guName = "Gang-nam"
+                )
+                val parkingLotInfoDTO = ParkingLotInfoDTO.from(parkingLotInfoVO)
 
+                every { createParkingLotUseCase.create(any()) } returns parkingLotInfoVO
+
+                it("생성한 경우가 나와야 한다.") {
+                    val result = mockMvc.perform(
+                        MockMvcRequestBuilders.post("/parking-lot/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsString(parkingLotInfoDTO))
+                    )
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(MockMvcResultMatchers.status().isOk)
+                        .andReturn()
+
+
+                    val responseBody =
+                        mapper.readValue(result.response.contentAsByteArray, SuccessResponseDTO::class.java)
+
+
+                    val realResult =
+                        mapper.convertValue(responseBody.content, ParkingLotInfoDTO::class.java)
+
+                    realResult shouldBe parkingLotInfoDTO
+                }
+            }
+        }
     }
 }
