63263308c4c39c6d81016a246cae7998aed5c343
acecic82 <102938al@naver.com>
2024-10-27 06:12:26 +0900

    MemberCommandController beforeTest 추가

---
+++ api/src/test/kotlin/com/parking/api/adapter/in/MemberCommandControllerTest.kt
@@ -0,0 +1,36 @@
+package com.parking.api.adapter.`in`
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.parking.api.application.port.`in`.member.ModifyMemberInfoUseCase
+import com.parking.api.application.port.`in`.member.RevokeMemberUseCase
+import com.parking.api.application.port.`in`.member.SaveMemberUseCase
+import com.parking.api.common.advice.ParkingAdvice
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.mockk
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+
+@SpringBootTest
+@ActiveProfiles(profiles = ["local","persistence-local"])
+class MemberCommandControllerTest(
+    private val mapper: ObjectMapper
+) : DescribeSpec() {
+    init {
+        lateinit var mockMvc: MockMvc
+        val saveMemberUseCase = mockk<SaveMemberUseCase>()
+        val modifyMemberInfoUseCase = mockk<ModifyMemberInfoUseCase>()
+        val revokeMemberUseCase = mockk<RevokeMemberUseCase>()
+
+        this.beforeTest {
+            mockMvc = MockMvcBuilders.standaloneSetup(
+                MemberCommandController(saveMemberUseCase, modifyMemberInfoUseCase, revokeMemberUseCase)
+            )
+                .setControllerAdvice(
+                    ParkingAdvice::class.java
+                )
+                .build()
+        }
+    }
+}
